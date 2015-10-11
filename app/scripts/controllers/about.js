2fe833fe02d82075075315ac29d96303710a79b8
Eugene <eugene.beloff@gmail.com>
2015-10-11 19:36:57 +0300

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngYsuraTaskApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngYsuraTaskApp
+ */
+angular.module('ngYsuraTaskApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngYsuraTaskApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngYsuraTaskApp
+ */
+angular.module('ngYsuraTaskApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
